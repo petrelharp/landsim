4f8e2962d7ad65caef013c0bf2c17603811e3393
peter <petrel.harp@gmail.com>
2015-12-21 01:19:04 -0800

    .

---
+++ R/sierpinski.R
@@ -0,0 +1,47 @@
+#' Overlay a Raster with a Sierpinski Mask
+#'
+#' This constructs a "middle-ninths" Sierpinski gasket of depth n,
+#' starting from a given Raster*.
+#'
+#' @param layer A Raster* object.
+#' @param box The bounding box to Cantor-ize.
+#' @param n The number of iterations.
+#' @param random Choose edges randomly or at (1/3,2/3)?
+#' @export
+#' @return A Raster* of the same form as the input.
+sierpinski_overlay <- function (
+                                layer,
+                                box=bbox(layer),
+                                n=floor(log(max(nrow(layer),ncol(layer))/log(3))),
+                                random=FALSE
+                        ) {
+    if (random) {
+        mid.s1 <- sort( c( box[1,], runif(8,min=box[1,1],max=box[1,2]) ) )[c(1,4,7,10)]
+        mid.s2 <- sort( c( box[2,], runif(8,min=box[2,1],max=box[2,2]) ) )[c(1,4,7,10)]
+    } else {
+        mid.s1 <- sort( c( box[1,], box[1,1]+c(1,2)*diff(box[1,])/3 ) )
+        mid.s2 <- sort( c( box[2,], box[2,1]+c(1,2)*diff(box[2,])/3 ) )
+    }
+    # remove the middle ninth
+    cutbox.coords <- cbind( s1=mid.s1[1+c(1,2,2,1,1)],
+                            s2=mid.s2[1+c(1,1,2,2,1)] )
+    cutpoly <- Polygon( coords=cutbox.coords )
+    cutpoly.sp <- SpatialPolygons( list( Polygons( list( cutpoly ), ID="cutbox" ) ) )
+    cutmask <- rasterize( cutpoly.sp, layer, background=NA )
+    layer <- mask( layer, cutmask, inverse=TRUE )
+    # iterate 
+    if (n>1) {
+        for (ii in 1:3) {
+            for (jj in 1:3) {
+                if ( (ii!=2) || (jj!=2) ) {
+                    thisbox <- rbind( mid.s1[c(ii,ii+1)],
+                                      mid.s2[c(jj,jj+1)] )
+                    layer <- sierpinski_overlay( layer=layer, box=thisbox, n=n-1, random=random )
+                }
+            }
+        }
+    }
+    return(layer)
+}
+
+
