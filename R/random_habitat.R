ff08bcb67a25a30a1a1e3ca3dc2af9bf29981e28
peter <petrel.harp@gmail.com>
2015-12-25 11:16:35 -0800

    .

---
+++ R/random_habitat.R
@@ -0,0 +1,28 @@
+#' Simulate a Random Environment
+#'
+#' Simulates a random environment, with holes and varying carrying capacity.
+#'
+#' @param diam Diameter of the desired habitat.
+#' @param res Spatial resolution of the Raster.
+#' @param randfun Function used to sample values for the habitat (negative values will be set to NA).
+#' @param kern Smoothing kernel.
+#' @param sigma Smoothing kernel scale.
+#' @param radius Smoothing kernel maximum radius.
+#' @export
+#' @return A RasterLayer with nonnegative and missing values.
+random_habitat <- function ( diam=1e4, 
+                            res=100, 
+                            randfun=function(n)pmin(20,(2+rcauchy(n))),
+                            kern="gaussian", 
+                            sigma=300, 
+                            radius=1500
+                            ) {
+
+    habitat <- raster(xmn=-diam, xmx=diam, ymn=-diam, ymx=diam, 
+          resolution=res,
+          crs="+proj=utm +zone=11 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+    values(habitat) <- randfun(ncell(habitat))
+    habitat <- 20*migrate_raster( habitat, kern=kern, sigma=sigma, radius=radius )
+    values(habitat)[values(habitat)<0] <- NA
+    return(habitat)
+}
