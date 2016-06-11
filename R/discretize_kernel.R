f67ff84685e9d85aa4dc809a16f5d54683e9ce28
peter <petrel.harp@gmail.com>
2016-06-11 07:47:37 -0700

    random version of discretize_kernel

--- R/discretize_kernel.R
+++ R/discretize_kernel.R
@@ -1,16 +1,20 @@
 #' Create a "Discretized" Migration Function from a Continuous One
 #'
-#' Given a function kern(x,y) that gives per-individual migration rates and a resolution, 
+#' Given a function kern(r) that gives per-individual migration rates and a resolution, 
 #' returns the (approximate) function giving per-individual migration rates *per unit area*
-#' from squares of side length `resolution` centered at (x,y).
+#' from squares of side length `resolution` centered at distance r.  (This depends on the 
+#' direction the cell is in, but the result averages over directions.)
 #'
 #' @param kern The migration rate function.
 #' @param res The resolution of the grid.
 #' @param radius The maximum distance to extrapolate out to.
 #' @param sigma Distance scaling for kernel.
 #' @param fact The number of divisions of each grid cell side when approximating the integral.
+#' @param sim Whether to obtain the result by Monte Carlo integration (currently only implemented for the Gaussian kernel).
 #' @export
-#' @return A function as output by `approxfun()`.
+#' @return A function as output by `approxfun()`, determined by numerical integration.
+#' If the required number of grid cells in the approximation is too large,
+#' falls back on Monte Carlo integration.
 #'
 #' The result is designed to be used in migration_matrix, which scales input to the kernel by sigma
 #' and assumes that the kernel is a density function, so multiplies the value by the area of a cell; 
@@ -22,7 +26,8 @@ discretize_kernel <- function (kern,
                                res, 
                                radius, 
                                sigma=1, 
-                               fact=10
+                               fact=10,
+                               sim=(kern=="gaussian"&&fact>10)
                            ) {
     cell.radius <- max(ceiling(radius/res))
     stencil <- raster::raster( xmn=-(cell.radius+0.5)*res[1], 
@@ -31,35 +36,50 @@ discretize_kernel <- function (kern,
                                ymx=(cell.radius+0.5)*res[2],
                                resolution=res,
                                # without CRS string coordinates are lonlat not meters
-                               crs=CRS("+proj=utm +datum=NAD83 +units=m") )
+                               crs=sp::CRS("+proj=utm +datum=NAD83 +units=m") )
     distvec <- values( raster::distanceFromPoints( stencil, c(0,0) ) )
-    stencil.fine <- raster::disaggregate(stencil, fact=fact)
-    distvec.fine <- raster::values( raster::distanceFromPoints( stencil.fine, c(0,0) ) )
-    # which new cells do old cell centers fall in
-    fine.locs <- raster::xyFromCell(stencil.fine,seq_along(stencil.fine))
-    fine.in.coarse <- raster::cellFromXY(stencil,fine.locs)
     center.loc <- raster::cellFromXY(stencil,c(0,0))
-    M.fine <- migration_matrix( stencil.fine, 
-                       accessible=rep(TRUE,length(stencil.fine)),
-                       kern=kern, 
-                       sigma=sigma,
-                       radius=radius, 
-                       normalize=NULL,
-                       from=which(fine.in.coarse==center.loc), 
-                       to=seq_along(stencil.fine) )
-    M.aggr <- aggregate_migration( M.fine, 
-                       old=stencil.fine, 
-                       new=stencil,
-                       from.old=which(fine.in.coarse==center.loc), 
-                       to.old=seq_along(stencil.fine),
-                       from.new=center.loc, 
-                       to.new=seq_along(stencil) )
+    if (!sim) {
+        stencil.fine <- raster::disaggregate(stencil, fact=fact)
+        distvec.fine <- raster::values( raster::distanceFromPoints( stencil.fine, c(0,0) ) )
+        # which new cells do old cell centers fall in
+        fine.locs <- raster::xyFromCell(stencil.fine,seq_along(stencil.fine))
+        fine.in.coarse <- raster::cellFromXY(stencil,fine.locs)
+        M.fine <- migration_matrix( stencil.fine, 
+                           accessible=rep(TRUE,length(stencil.fine)),
+                           kern=kern, 
+                           sigma=sigma,
+                           radius=radius, 
+                           normalize=NULL,
+                           from=which(fine.in.coarse==center.loc), 
+                           to=seq_along(stencil.fine) )
+        M.aggr <- aggregate_migration( M.fine, 
+                           old=stencil.fine, 
+                           new=stencil,
+                           from.old=which(fine.in.coarse==center.loc), 
+                           to.old=seq_along(stencil.fine),
+                           from.new=center.loc, 
+                           to.new=seq_along(stencil) )
+        M.probs <- as.vector(M.aggr)
+    } else {
+        npts <- 1e6
+        xy0 <- cbind( 
+                 x=(runif(npts)-0.5)*cell.radius*res[1],
+                 y=(runif(npts)-0.5)*cell.radius*res[2]
+             )
+        xy1 <- cbind(
+                 x=xy0[,"x"] + rnorm(npts,sd=sigma),
+                 y=xy0[,"y"] + rnorm(npts,sd=sigma)
+             )
+        M.probs <- as.vector(tabulate( raster::cellFromXY( stencil, SpatialPoints( xy1, proj4string=sp::CRS(sp::proj4string(stencil)) ) ), nbins=prod(dim(stencil)) ))/npts
+    }
     #   must divide by the target area to get a density function
     # outfun <- approxfun( distvec/sigma, as.vector(M.aggr)/prod(res/sigma), rule=2 )
     out.ord <- order(distvec) # y must be increasing for method='hyman'
-    use.these <- c(TRUE, diff(distvec[out.ord])/diff(range(distvec)) > 1e-3 )
+    # use.these <- c(TRUE, diff(distvec[out.ord])/diff(range(distvec)) > 1e-3 )
+    use.these <- TRUE   # if not using 'hyman' don't need to remove redundants
     outfun <- splinefun( distvec[out.ord][use.these]/sigma, 
-                        as.vector(M.aggr)[out.ord][use.these]/prod(res/sigma), 
+                        M.probs[out.ord][use.these]/prod(res/sigma), 
                         method="monoH.FC" )
     attr(outfun,"res") <- res
     attr(outfun,"sigma") <- sigma
--- tests/testthat/test_discretize_kernel.R
+++ tests/testthat/test_discretize_kernel.R
@@ -4,11 +4,13 @@ context("testing kernel discretization")
 # Discretization should be finer than sigma:
 # it seems to be enough so that res/fact = sigma/3, i.e., if fact = 3*res/sigma
 
+###### test non-simulation method
+
 ### first test at a not-terribly-small value of sigma
 
 sigma <- 0.1
 res <- 1
-f <- function (fact) { discretize_kernel('gaussian', res=c(res,res), radius=3*sigma, sigma=sigma, fact=fact) }
+f <- function (fact) { discretize_kernel('gaussian', res=c(res,res), radius=3*sigma, sigma=sigma, fact=fact, sim=FALSE) }
 fact.vec <- c(5,10,15,20,25,30)
 names(fact.vec) <- fact.vec
 f.list <- lapply( fact.vec, f )
@@ -56,7 +58,7 @@ expect_true(
 
 sigma <- 0.01
 res <- 1
-f <- function (fact) { discretize_kernel('gaussian', res=c(res,res), radius=3*sigma, sigma=sigma, fact=fact) }
+f <- function (fact) { discretize_kernel('gaussian', res=c(res,res), radius=3*sigma, sigma=sigma, fact=fact, sim=FALSE) }
 fact.vec <- (1:3)*res/sigma
 names(fact.vec) <- fact.vec
 f.list <- lapply( fact.vec, f )
@@ -77,4 +79,81 @@ results <- rbind(
 expect_true( 
             all( abs(results[,names(fact.vec)[length(fact.vec)]]-results[,"sim"]) < 5*sqrt(1e6*results[,"sim"]) ) 
         )
+
+
+####### now test Monte Carlo method
+
+### first test at a not-terribly-small value of sigma
+
+sigma <- 0.1
+res <- 1
+f <- function (fact) { discretize_kernel('gaussian', res=c(res,res), radius=3*sigma, sigma=sigma, fact=fact, sim=TRUE) }
+fact.vec <- c(5,10,15,20,25,30)
+names(fact.vec) <- fact.vec
+f.list <- lapply( fact.vec, f )
+
+# check at one value
+x.ends <- seq(-0.5,0.5,length.out=51)
+xx <- x.ends[-1]-diff(x.ends)/2
+dx <- mean(diff(xx))
+
+# offset by (1,0)
+right.grid <- expand.grid( 
+                         x0=xx, y0=xx,
+                         x1=1+xx, y1=0+xx
+                     )
+right.grid$z <- dx^2 * (dnorm(right.grid$x1-right.grid$x0,sd=sigma)*dnorm(right.grid$y1-right.grid$y0,sd=sigma))
+right.check <- sum(right.grid$z)/length(xx)^2
+
+# simulation check
+sim <- data.frame( x0=runif(1e6)-1/2, y0=runif(1e6)-1/2 )
+sim$x1 <- sim$x0 + rnorm(1e6,sd=sigma)
+sim$y1 <- sim$y0 + rnorm(1e6,sd=sigma)
+sim.counts <- table( cut(sim$x1,((-1:2)-1/2)*res), 
+                     cut(sim$y1,((-1:2)-1/2)*res) )
+
+# offset by (1,1)
+diag.grid <- expand.grid( 
+                         x0=xx, y0=xx,
+                         x1=1+xx, y1=1+xx
+                     )
+diag.grid$z <- dx^2 * (dnorm(diag.grid$x1-diag.grid$x0,sd=sigma)*dnorm(diag.grid$y1-diag.grid$y0,sd=sigma))
+diag.check <- sum(diag.grid$z)/length(xx)^2
+
+# compare
+results <- rbind(
+    'right'=c( sapply( f.list, function (g) g(1/sigma)*res^2/sigma^2 ), check=right.check, sim=mean(sim.counts[c(2,4,6,8)])/1e6 ),
+    'diag'=c( sapply( f.list, function (g) g(sqrt(2)/sigma)*res^2/sigma^2 ), check=diag.check, sim=mean(sim.counts[c(1,3,7,9)])/1e6 )
+    )
+
+expect_true( 
+            all( abs(results[,"30"]-results[,"sim"]) < 5*sqrt(1e6*results[,"sim"]) ) 
+            && all( abs(results[,"30"]-results[,"check"]) < 5*sqrt(1e6*results[,"check"]) ) )
+
+### now test at a smaller value of sigma
+
+sigma <- 0.01
+res <- 1
+f <- function (fact) { discretize_kernel('gaussian', res=c(res,res), radius=3*sigma, sigma=sigma, fact=fact, sim=TRUE) }
+fact.vec <- (1:3)*res/sigma
+names(fact.vec) <- fact.vec
+f.list <- lapply( fact.vec, f )
+
+# simulation check
+sim <- data.frame( x0=runif(1e6)-1/2, y0=runif(1e6)-1/2 )
+sim$x1 <- sim$x0 + rnorm(1e6,sd=sigma)
+sim$y1 <- sim$y0 + rnorm(1e6,sd=sigma)
+sim.counts <- table( cut(sim$x1,((-1:2)-1/2)*res), 
+                     cut(sim$y1,((-1:2)-1/2)*res) )
+
+# compare
+results <- rbind(
+    'right'=c( sapply( f.list, function (g) g(1/sigma)*res^2/sigma^2 ), sim=mean(sim.counts[c(2,4,6,8)])/1e6 ),
+    'diag'=c( sapply( f.list, function (g) g(sqrt(2)/sigma)*res^2/sigma^2 ), sim=mean(sim.counts[c(1,3,7,9)])/1e6 )
+    )
+
+expect_true( 
+            all( abs(results[,names(fact.vec)[length(fact.vec)]]-results[,"sim"]) < 5*sqrt(1e6*results[,"sim"]) ) 
+        )
+
 
