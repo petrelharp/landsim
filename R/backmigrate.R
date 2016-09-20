6d0a0c13cb19a8f7a4f5cb2228c414c5a1a3b172
peter <petrel.harp@gmail.com>
2016-09-20 15:25:35 -0700

    can sample lineages back one step

---
+++ R/backmigrate.R
@@ -0,0 +1,80 @@
+#' Sample from Parental Locations
+#'
+#' This takes a set of locations and a nonnegative weighting vector,
+#' and samples from the time-reversed migration probabilities, weighted by the weights.
+#'
+#' @param x A vector of locations (indices).
+#' @param migration The \code{migration} object.
+#' @param weights A nonnegative vector of weights.
+#' @export
+#' @return A vector of locations of the same form as \code{x}.
+#' If the probability of migrating from i to j is M[i,j], then a sample at j will choose i
+#' with probability equal to \code{weights[i] M[i,j]/sum(weights*M[,j])}.
+backmigrate <- function ( x,
+                      migration,
+                      weights
+                 ) {
+    if (is.null(migration$M)) { stop("Migration matrix does not exist: need to use setup_demography, or migrate_raster()?") }
+
+    # prob.step[i,k] will contain the probability of migrating from i to x[k]
+    #  in however many steps we've taken so far
+    prob.step <- sparseMatrix( i=x, j=seq_along(x), x=rep(1.0,length(x)), 
+                          dims=c(nrow(migration$M),ncol=length(x)) )
+
+    zero.weight <- 1-sum(migration$n.weights)
+    if (zero.weight<0) { warning("n.weights sum to more than one.") }
+    probs <- zero.weight * prob.step
+    for (n in seq_along(migration$n.weights)) {
+        # note M is row-stochastic, so %*% does the right thing
+        prob.step <- migration$M %*% prob.step 
+        if (migration$n.weights[n]!=0) {
+            probs <- probs + migration$n.weights[n]*prob.step
+        }
+    }
+    # return samples
+    probs <- sweep( probs, 1, weights, "*" )
+    out <- apply( probs, 2, function (pp) { sample.int( n=nrow(probs), size=1, prob=pp ) } )
+    return( as(out,class(x)) )
+}
+
+#' Sample from Parental Genotypes
+#'
+#' This takes a mating tensor, a vector of genotypes, a vector of weights, 
+#' and matrices of numbers of possible seed and pollen parents (with seed[k,j] and pollen[k,j] 
+#' being the numbers of possible j-genotype parents of genotype[k]),
+#' and samples from parental genotypes.  
+#'
+#' @param x A vector of genotypes.
+#' @param mating A mating tensor.
+#' @param weights A numeric vector of length equal to number of genotypes.
+#' @param seed A matrix with dimensions \code{length(x)} by number of genotypes.
+#' @param pollen A matrix with dimensions \code{length(x)} by number of genotypes.
+#' @export
+#' @return A \code{length(x)} by 2 matrix whose first column is the genotype index 
+#' and the second column is 1 or 2, meaning:
+#'   1 : seed (first dimension in mating tensor)
+#'   2 : pollen (second dimension in mating tensor)
+#'
+#' For genotype=k, the probability 
+#' of choosing parent genotype pair i,j
+#' is proportional to 
+#'   \code{X[i,j] = mating[i,j,k]*seed[i]*pollen[j]}
+#' and given \code{i,j}, the probability of choosing the seed parent is equal to
+#'   \code{weights[i]/(weights[i]+weights[j])} .
+backmeiosis <- function ( x,
+                         mating,
+                         weights,
+                         seed,
+                         pollen
+                     ) {
+    n.genotypes <- dim(mating)[1]
+    # premultiply by weights
+    out <- matrix( nrow=length(x), ncol=2 )
+    for (k in seq_along(x)) {
+        X <- seed[k,] *  sweep( mating[,,x[k]], 2, pollen[k,], "*" )
+        ij <- arrayInd( sample.int(length(X), size=1, prob=X), .dim=dim(X) )
+        ps <- sample.int( 2, size=1, prob=weights[ij] )
+        out[k,] <- c( ij[ps], ps )
+    }
+    return(out)
+}
---
+++ R/lineage_generation.R
@@ -0,0 +1,62 @@
+#' Simulate a Single Backwards Generation
+#'
+#' Given a collection of lineages, all of the same genotype,
+#' sample from their location in the previous generation,
+#' given the entire set of reproductive numbers in that generation.
+#'
+#' @param lineages A two-column matrix with columns labeled 'location' and 'genotype'.
+#' @param N The population state at the start of the previous generation.
+#' @param gen A list as returned by \code{generation(...,return.everything=TRUE)}.
+#' @param num.alleles A vector giving the number of alleles of the type followed in each genotype.
+#' @param debug Whether to run sanity checks.
+#' @export
+#' @return A matrix of the same form as \code{lineages}.
+lineage_generation <- function (lineages, N, gen, num.alleles, debug=FALSE) {
+
+    # current generation
+    now.N <- N - gen$death + gen$germination
+
+    # the youth will move
+    is.youth <- ( 0 < rbinom( nrow(lineages), 
+                         size=1, prob=gen$germination[lineages]/now.N[lineages] ) )
+    youth <- lineages[is.youth,,drop=FALSE]
+
+    if (any(is.youth)) {
+
+        # do backwards seed migration for youth
+        for (k in unique(youth[,'genotype'])) {
+            do.these <- ( youth[,'genotype']==k )
+            youth[do.these,'location'] <- backmigrate( youth[do.these,'location'], 
+                                                  demog$seed.migration, weights=gen$seed.production[,k] )
+        }
+
+        # choose seed or pollen parent
+        num.seed.parents <- gen$seeders[youth[,'location'],]
+        num.pollen.parents <- gen$pollen[youth[,'location'],]
+        # check this was possible
+        if (debug) {
+            stopifnot(all(rowSums(num.seed.parents)>0))
+            stopifnot(all(rowSums(num.pollen.parents)>0))
+        }
+
+        parent.types <- backmeiosis( youth[,'genotype'], mating=demog$mating, weights=num.alleles, 
+                                    seed=num.seed.parents, pollen=num.pollen.parents )
+        colnames(parent.types) <- c("genotype","which")
+
+        # copy back over lineages that chose 'seed' parents
+        youth[,'genotype'] <- parent.types[,'genotype']
+        is.pollen <- ( parent.types[,2]==2 )
+
+        # backmigrate pollen parents
+        for (k in unique(youth[is.pollen,'genotype'])) {
+            do.these <- ( is.pollen & ( youth[,'genotype']==k ) )
+            youth[do.these,'location'] <- backmigrate( youth[do.these,'location'], demog$pollen.migration, weights=N[,k] )
+        }
+    }
+
+    # finally, find the previous generation's locations
+    parents <- lineages
+    parents[is.youth,] <- youth
+
+    return(parents)
+}
